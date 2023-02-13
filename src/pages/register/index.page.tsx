6071d3de7d2ac87b31d33ec96a7fe32ee649a728
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-13 14:24:31 -0300

    bring username to register page

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Container, Form, FormError, Header } from "./styles";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const registerFormSchema = z.object({
   username: z
@@ -30,10 +32,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<registerFormData>({
     resolver: zodResolver(registerFormSchema),
   });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue("username", String(router.query.username));
+    }
+  }, [router?.query?.username, setValue]);
 
   async function handleRegister(data: registerFormData) {}
 
