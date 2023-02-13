d0919f00855d4bc196e91ce657817b9d2e761d23
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-13 15:45:02 -0300

    connect calendar - step2

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,37 @@
+import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+
+import { Container, Header } from "./../styles";
+import { ConnectBox, ConnectItem } from "./styles";
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda</Heading>
+        <Text>
+          {" "}
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit" disabled>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  );
+}
