9ad7af05d78d584ba88dec7ee1208a0c988dded0
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-13 14:53:37 -0300

    validation POST method

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -2,5 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
   return res.status(200).json(req.body);
 }
