b058507b3c93861c23ce88b847f47cc42f5dc0bd
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-13 14:55:50 -0300

    creating new user

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,10 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../libs/prisma";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  return res.status(200).json(req.body);
+  const { name, username } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  });
+
+  return res.status(201).json(user);
 }
