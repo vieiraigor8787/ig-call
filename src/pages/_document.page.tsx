78c5f060c764a81e38dbd156fdc3cf42aa2e654b
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-13 10:09:10 -0300

    adjustments

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,5 +1,5 @@
-import { getCssText } from "@ignite-ui/react";
-import { Head, Html, Main, NextScript } from "next/document";
+import { getCssText } from '@ignite-ui/react'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -26,5 +26,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
