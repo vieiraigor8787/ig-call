59270deedfbfb7f49da781e58a2cded338ad7c79
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-23 15:17:13 -0300

    using lib next-seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,9 +1,11 @@
 import "../libs/dayjs";
-
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { globalStyles } from "../../styles/global";
+import { DefaultSeo } from "next-seo";
 import { QueryClientProvider } from "@tanstack/react-query";
+
+import { globalStyles } from "../../styles/global";
+
 import { queryClient } from "../libs/react-query";
 
 globalStyles();
@@ -15,6 +17,19 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: "website",
+            locale: "pt_BR",
+            url: "https://ig-call.igapplications.com/",
+            siteName: "Ig Call",
+          }}
+          twitter={{
+            handle: "@handle",
+            site: "@site",
+            cardType: "summary_large_image",
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
