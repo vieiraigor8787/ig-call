837b9b99c3eaae05fcb8b44d1f5771c9b5ad7885
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-02-23 11:56:13 -0300

    validating schedule and create at database

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,4 +1,6 @@
+import dayjs from "dayjs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
 import { prisma } from "../../../../libs/prisma";
 
 export default async function handler(
@@ -21,5 +23,45 @@ export default async function handler(
     return res.status(400).json({ message: "Usuário não existente." });
   }
 
-  return res.json({});
+  const createSchedulingBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    obs: z.string(),
+    date: z.string().dateTime(),
+  });
+
+  const { date, name, email, obs } = createSchedulingBody.parse(req.body);
+
+  const schedulingDate = dayjs(date).startOf("hour");
+
+  if (schedulingDate.isBefore(new Date())) {
+    return res.status(400).json({
+      message: "A data escolhida já passou",
+    });
+  }
+
+  const conflictingSchedule = await prisma.schedule.findFirst({
+    where: {
+      user_id: user.id,
+      date: schedulingDate.toDate(),
+    },
+  });
+
+  if (conflictingSchedule) {
+    return res.status(400).json({
+      message: "A data escolhida já foi agendada.",
+    });
+  }
+
+  await prisma.schedule.create({
+    data: {
+      name,
+      email,
+      observations: obs,
+      date: schedulingDate.toDate(),
+      user_id: user.id,
+    },
+  });
+
+  return res.status(201).end();
 }
